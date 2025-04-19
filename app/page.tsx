efef0e2ac041f25ccad92985c62ac3a433576763
Matias <lit.io30303@gmail.com>
2025-04-18 20:35:39 -0400

    delete coverparticle from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { CoverParticles } from "@/components/cover-particles"
 import { Introduction } from "@/components/introduction";
 import { TransitionPage } from "@/components/transition-page";
 
